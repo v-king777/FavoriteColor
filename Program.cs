7576b95e60c6bc5637554eedc2d67772a4279447
v-king777 <v.kolpashchikov@gmail.com>
2021-01-19 11:09:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FavoriteColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
